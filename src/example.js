65896dd5549b5aa4bd25179392e5b9882d959bff
edgfb-249 <0cv0lg28@136329.xyz>
2026-01-10 01:15:04 +0800

    Add ESLint configuration, workflow, and failing example

---
+++ src/example.js
@@ -0,0 +1,7 @@
+// This file intentionally contains linting errors
+
+function unusedFunction(param1, param2) {
+  console.log("This uses double quotes and will trigger linting errors")
+}
+
+unusedFunction(1);
